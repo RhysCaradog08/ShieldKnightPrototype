86b17031f40f896b599faa63a949f716bf6f29c5
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2022-02-08 16:39:51 +0000

    Added Wave Shield Attack Animations
    Created and added Wave Shield AttackLeft, AttackRight and AttackOverhead animations.
    
    Added basic code to WaveShieldController.cs to track and control attack int.

--- ShieldKnightPrototype/Assets/Scripts/Shields/WaveShield/WaveShieldController.cs
+++ ShieldKnightPrototype/Assets/Scripts/Shields/WaveShield/WaveShieldController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class WaveShieldController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int attackCount;
+    [SerializeField] float attackDelay, attackReset;
+
+    private void Start()
     {
-        
+        attackCount = 3;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetButtonDown("Throw") && attackDelay <= 0)
+        {
+            if(attackCount > 0)
+            {
+                attackCount--;
+                attackDelay = 1f;
+                attackReset = 2f;
+            }
+        }
+
+        /*if(attackCount <= 0)
+        {
+            attackCount = 0;
+        }*/
+
+        if(attackDelay > 0)
+        {
+            attackDelay -= Time.deltaTime;
+        }
+
+        if(attackDelay <= 0)
+        {
+            attackDelay = 0;
+        }
+
+        if(attackReset > 0)
+        {
+            attackReset -= Time.deltaTime;
+        }
+
+        if(attackReset <= 0)
+        {
+            attackReset = 0;
+            attackCount = 3;
+        }
     }
 }
