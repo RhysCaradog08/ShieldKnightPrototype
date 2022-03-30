2bb9c6cf711e3447cf2669b723bdc5480bd48843
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2022-03-30 16:02:18 +0100

    Update GrindRail.cs

--- ShieldKnightPrototype/Assets/GrindRail.cs
+++ ShieldKnightPrototype/Assets/GrindRail.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GrindRail : MonoBehaviour
 {
     [SerializeField] List<Transform> grindPositions = new List<Transform>();
+
     // Start is called before the first frame update
     void Start()
     {
