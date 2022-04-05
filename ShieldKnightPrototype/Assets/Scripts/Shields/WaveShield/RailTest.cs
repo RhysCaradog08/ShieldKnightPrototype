6e55e8cc0107562e8e96a20ccf656a04b567fe8b
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2022-04-05 14:36:07 +0100

    Revert "Iteration of Grind Feature"
    This reverts commit c331683525e5359fc4d7119ef04af6d256d935bb.

--- ShieldKnightPrototype/Assets/Scripts/Shields/WaveShield/RailTest.cs
+++ ShieldKnightPrototype/Assets/Scripts/Shields/WaveShield/RailTest.cs
@@ -10,25 +10,12 @@ public class RailTest : MonoBehaviour
     [SerializeField] Collider[] grindObjects;
 
     [SerializeField] private int currentSegment;
-    [SerializeField] private float transition, activateDistance;
-    [SerializeField] List<Transform> grindPoints = new List<Transform>();
-    [SerializeField] private Transform closest;
-    [SerializeField] int index;
+    [SerializeField] private float transition;
     public float speed = 5;
 
-    [Header("Activation Bools")]
-    public bool isCompleted, canGrind, isGrinding;
-
-    [Header("Control Bools")]
+    public bool isCompleted;
     [SerializeField] private bool isReversed;
     [SerializeField] private bool isLooping;
-
-    private void Start()
-    {
-        isCompleted = false;
-        canGrind = false;
-        isGrinding = false;
-    }
 
     private void Update()
     {
@@ -37,40 +24,24 @@ public class RailTest : MonoBehaviour
         if (!rail)
             return;
 
-        if (activateDistance < 2f && !isGrinding)
-        {
-            canGrind = true;
-        }
-        else if(isGrinding)
-        {
-            canGrind = false;
-        }
-
-        if (!isCompleted && canGrind)
-            Play();
-
-        else if(isCompleted)
-        {
-            isGrinding = false;
-        }
+        if (!isCompleted)
+            Play(!isReversed);
     }
 
     void Play(bool forward = true)
     {
-        currentSegment = index;
-
         float m = (rail.nodes[currentSegment + 1].position - rail.nodes[currentSegment].position).magnitude;
         float s = (Time.deltaTime * 1 / m) * speed;
         transition += (forward)? s : -s ;
 
-        if (transition > 1)
+        if(transition > 1)
         {
             transition = 0;
             currentSegment++;
 
-            if (currentSegment == rail.nodes.Length - 1)
+            if(currentSegment == rail.nodes.Length -1)
             {
-                if (isLooping)
+                if(isLooping)
                 {
                     currentSegment = 0;
                 }
@@ -81,7 +52,7 @@ public class RailTest : MonoBehaviour
                 }
             }
         }
-        else if (transition < 0)
+        else if(transition < 0)
         {
             transition = 1;
             currentSegment--;
@@ -111,51 +82,29 @@ public class RailTest : MonoBehaviour
 
         foreach (Collider col in grindObjects)
         {
+            Debug.DrawLine(transform.position, col.transform.position, Color.red);
+            Debug.LogFormat(col.name);
+
             if (col.tag == "Grind")
             {
-                rail = col.gameObject.GetComponent<GrindRail>();
+                Debug.DrawLine(transform.position, col.transform.position, Color.yellow);
+                float distance = Vector3.Distance(col.transform.position, transform.position);
 
-                grindPoints = new List<Transform>(rail.nodes);
-                GetClosestGrindPoint();
-                
-                foreach(Transform gp in grindPoints)
+                if (distance < 2f)
                 {
-                    Debug.DrawLine(transform.position, gp.position, Color.red);
-                    Debug.DrawLine(transform.position, closest.position, Color.yellow);
+                    Debug.Log("Grind Object:  " + col.name);
+                    Debug.DrawLine(transform.position, col.transform.position, Color.green);
+
+                    rail = col.gameObject.GetComponent<GrindRail>();
                 }
-                activateDistance = Vector3.Distance(closest.position, transform.position);
             }
         }
     }
 
-    void GetClosestGrindPoint()
-    {
-        grindPoints.Sort(delegate (Transform a, Transform b)
-        {
-            return Vector3.Distance(transform.position, a.transform.position)
-            .CompareTo(
-              Vector3.Distance(transform.position, b.transform.position));
-        });
+   void FindNearestNode()
+   {
 
-        closest = null;
-        float closestDistanceSqr = Mathf.Infinity;
-        Vector3 currentPosition = transform.position;
-
-        foreach(Transform gPoint in grindPoints)
-        {
-            Vector3 directionToClosest = gPoint.position - currentPosition;
-            float dSqrToTarget = directionToClosest.sqrMagnitude;
-
-            if (dSqrToTarget < closestDistanceSqr)
-            {
-                closestDistanceSqr = dSqrToTarget;
-                closest = gPoint;
-
-                index = System.Array.IndexOf(rail.nodes, closest);
-                Debug.Log("Index: " + index);
-            }
-        }
-    }
+   }
 
     void OnDrawGizmosSelected()
     {
