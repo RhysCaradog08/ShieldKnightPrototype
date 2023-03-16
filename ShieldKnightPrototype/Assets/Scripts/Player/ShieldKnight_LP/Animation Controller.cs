c7ec4c2f5efca18ce606318c0aaf24708a4e5531
RhysCaradog08 <55874291+RhysCaradog08@users.noreply.github.com>
2023-03-16 16:18:27 +0000

    Animation Duplication
    Due to Blender not exporting animation files correctly I duplicated ShieldKnight_Idle/Move/Throw/Parry so they are not lost with further export attempts.

--- ShieldKnightPrototype/Assets/Scripts/Player/ShieldKnight_LP/Animation Controller.cs
+++ ShieldKnightPrototype/Assets/Scripts/Player/ShieldKnight_LP/Animation Controller.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
+    [SerializeField] Animator anim;
+    [SerializeField] string currentState;
+
+    //Animation States
+    public string idle = "ShieldKnight_Idle";
+    public string move = "ShieldKnight_Move";
+    public string throwing = "ShieldKnight_Throw";
+    public string jump = "ShieldKnight_Jump";
+    public string barge = "ShieldKnight_Barge";
+    public string guard = "ShieldKnight_Guard";
+    public string parry = "ShieldKnight_Parry";
+    public string slam = "ShieldKnight_Slam";
+
+
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +32,48 @@ public class AnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        /*float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        if (moveX != 0 || moveZ != 0)
+        {
+            ChangeAnimationState(move);
+        }
+        else
+        {
+            ChangeAnimationState(idle);
+        }
+
+        if (Input.GetButton("Throw"))
+        {
+            ChangeAnimationState(throwing);
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            ChangeAnimationState(jump);
+        }
+
+        if (Input.GetButton("Barge"))
+        {
+            ChangeAnimationState(barge);
+        }
+
+        if (Input.GetButton("Guard"))
+        {
+            ChangeAnimationState(guard);
+        }*/
+    }
+
+    public void ChangeAnimationState(string newState)
+    {
+        //Stop the animation from interrupting itself.
+        if (currentState == newState) return;
+
+        //Play the animation.
+        anim.Play(newState);
+
+        //Reassign the current state.
+        newState = currentState;
     }
 }
---
+++ ShieldKnightPrototype/Assets/Scripts/Player/ShieldKnight_LP/Shield Knight Controller.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldKnightController : MonoBehaviour
+{
+    AnimationController animControl;
+
+    Camera cam;
+    Transform camPos;
+
+    [Header("Movement")]
+    CharacterController cc;
+    public float speed, moveSpeed, rotateSpeed;
+    public Vector3 move, moveDir;
+    public float turnSmoothTime;
+    float turnSmoothVelocity;
+    public bool canMove;
+
+    //Action Booleans
+    [SerializeField] bool isJumping, isThrowing, isBarging, isGuarding;
+
+    private void Awake()
+    {
+        animControl = FindObjectOfType<AnimationController>();
+
+        cc = GetComponent<CharacterController>();        
+        cam = Camera.main;
+        camPos = cam.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Movement
+        speed = moveSpeed;
+        turnSmoothTime = 0.1f;
+        canMove = true;
+
+        //Action Booleans
+        isJumping = false;
+        isThrowing = false;
+        isBarging = false;
+        isGuarding = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputCheck();
+
+        if (isJumping || isThrowing || isBarging || isGuarding)
+        {
+            canMove = false;
+        }
+        else canMove = true;
+
+        if(isJumping)
+        {
+            animControl.ChangeAnimationState(animControl.jump);
+        }
+
+        if (isThrowing)
+        {
+            animControl.ChangeAnimationState(animControl.throwing);
+        }
+
+        if (isBarging)
+        {
+            animControl.ChangeAnimationState(animControl.barge);
+        }
+
+        if (isGuarding)
+        {
+            animControl.ChangeAnimationState(animControl.guard);
+        }
+
+        if (canMove)
+        {
+            if (move.magnitude >= Mathf.Epsilon) //Orients the player to have forward orientation relevant to camera rotation.
+            {
+                float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + camPos.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                cc.Move(moveDir.normalized * speed * Time.deltaTime);
+                animControl.ChangeAnimationState(animControl.move);
+            }
+            else animControl.ChangeAnimationState(animControl.idle);
+        }
+    }
+
+    void InputCheck()
+    {
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        move = new Vector3(moveX, 0, moveZ).normalized;
+
+
+        if (Input.GetButton("Jump"))
+        {
+            isJumping = true;
+        }
+        else isJumping = false;
+
+        if (Input.GetButton("Throw"))
+        {
+            isThrowing = true;
+        }
+        else isThrowing = false;    
+
+        if (Input.GetButton("Barge"))
+        {
+            isBarging = true;
+        }
+        else isBarging = false;
+
+        if (Input.GetButton("Guard"))
+        {
+            isGuarding = true;
+        }
+        else isGuarding = false;
+    }
+}
